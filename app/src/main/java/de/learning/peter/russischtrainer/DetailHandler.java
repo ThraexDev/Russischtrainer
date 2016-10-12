fba628ac16ab66d4638c316e6b0a02f68ce99d16
Peter Truckenbrod <Peter Truckenbrod>
2016-10-12 09:57:51 +0200

    detail view

---
+++ app/src/main/java/de/learning/peter/russischtrainer/DetailHandler.java
@@ -0,0 +1,7 @@
+package de.learning.peter.russischtrainer;
+
+/**
+ * Created by Peter on 12.10.2016.
+ */
+public class DetailHandler {
+}
---
+++ app/src/main/java/de/learning/peter/russischtrainer/WordDetailActivity.java
@@ -0,0 +1,13 @@
+package de.learning.peter.russischtrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_detail);
+    }
+}
